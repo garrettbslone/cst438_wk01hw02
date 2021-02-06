e0fdeb709cade32efd186bf2d4d5a97a754e541e
Garrett Slone <gsloneca@gmail.com>
2021-02-05 19:01:14 -0800

    add intent factory test

--- app/src/androidTest/java/com/garrett/retrofit_setup/IntentFactoryTest.java
+++ app/src/androidTest/java/com/garrett/retrofit_setup/IntentFactoryTest.java
@@ -1,6 +1,8 @@
 package com.garrett.retrofit_setup;
 
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -16,11 +18,16 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith (AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class IntentFactoryTest {
     @Test
-    public void useAppContext () {
+    public void get_intent_factory () {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.garrett.retrofit_setup", appContext.getPackageName());
+
+        Intent intent = LandingPageActivity.intent_factory(appContext.getApplicationContext(),1);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        appContext.startActivity(intent);
+        assertEquals(1,intent.getIntExtra("uid", -1));
     }
 }
